e5e890044d94c0f85b6a036fa9fb330d4a87ff58
root <root@avish-docker.c.avinashgcp.internal>
2018-08-16 13:10:19 +0000

    config files

--- send/runrule.sh
+++ send/runrule.sh
@@ -6,4 +6,12 @@ mysql -h 127.0.0.1 -u $DB_USER -p$DB_PASSWORD -D jelly -e "select name from empl
 # Script to put the names from mysql output into a queue
 ./send.py
 
+sleep 10
+
+# Count the numbet of message
+JOB=`wc -w names.txt | awk '{print$1}'`
+
+# Trigger Jenkins JOBS - Batch Jobs to consume messages
+curl -v -X POST http://35.200.195.163:9090/job/kube-job-trigger/build --data token=batchjob --data-urlencode json='{"parameter": [{"name":"JOB", "value":'$JOB'}]}'
+
 sleep 100d
